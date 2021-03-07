814dd26f511f4d8e01a4aee0aea8d4aab4e40322
Cipiman1973 <62877816+Cipiman1973@users.noreply.github.com>
2021-03-07 20:50:17 +0100

    register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+      RouterModule.forRoot([
+        { path: '', component: RegisterComponent },
+        { path: 'reactive-form', component: RegisterComponent }
+      ]),
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+import {Validators, FormGroup, FormBuilder} from '@angular/forms';
+import {ServicesService} from '../services.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+// @ts-ignore
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private customValidator: ServicesService
+  ) {
+  }
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+        name: ['', Validators.required],
+        password: ['', Validators.compose([Validators.required, this.customValidator.patternValidator()])],
+        confirmPassword: ['', [Validators.required]],
+      },
+      {
+        validator: this.customValidator.MatchPassword('password', 'confirmPassword'),
+      }
+    );
+  }
+
+  get registerFormControl() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.valid) {
+      alert('Form Submitted! Values are in console');
+      console.log(this.registerForm.value);
+      this.registerForm.get('https://webhook.site/b6a51c16-5beb-42a5-86d9-55a927d24f32' + 'name is ' + name);
+    }
+  }
+
+}
